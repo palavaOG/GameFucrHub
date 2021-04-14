e5dcc3edda74472ec4c6621dcbf971677e38c4e7
palavaOG <80668782+palavaOG@users.noreply.github.com>
2021-04-14 19:07:24 +1000

    Create Script.lua

---
+++ Script/Script.lua
@@ -0,0 +1,9 @@
+if game.PlaceId == 4716045691 then -- polybattles
+loadstring(game:HttpGet('https://raw.githubusercontent.com/palavaOG/PolyFucr/main/Polyfcr-script/script.lua'))()
+else
+if game.PlaceId == 155615604 then -- Prisonlife
+loadstring(game:HttpGet('https://raw.githubusercontent.com/palavaOG/script-prisonfucr/main/prisonlife/prisonfucr.lua'))()
+else
+game.Players.LocalPlayer:kick("GameFucr: UnkownGame") 
+end
+end
